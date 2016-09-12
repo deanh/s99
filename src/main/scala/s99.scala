7197d346d58747acfc014afd05fcb0bcbe985d5e
Dean Hudson <dean@ero.com>
2016-09-13 00:30:53 +0200

    probs 2-4

--- src/main/scala/s99.scala
+++ src/main/scala/s99.scala
@@ -1,7 +1,35 @@
+import annotation.tailrec
+
 object s99 {
+  @tailrec
   def last[A](list: List[A]): Option[A] = list match {
     case Nil      => None
     case h :: Nil => Some(h)
     case h :: t   => last(t)
   }
+
+  @tailrec
+  def penultimate[A](list: List[A]): Option[A] = list match {
+    case Nil               => None
+    case h :: Nil          => None
+    case one :: two :: Nil => Some(one)
+    case h :: t            => penultimate(t)
+  }
+
+  @tailrec
+  def nth[A](n: Int, list: List[A]): Option[A] = (n, list) match {
+    case (_, Nil)    => None
+    case (0, h :: t) => Some(h)
+    case (i, h :: t) => nth(i - 1, t)
+  }
+
+  @tailrec
+  def length[A](list: List[A], cnt: Int = 0): Int = list match {
+    case Nil    => cnt
+    case h :: t => length(t, 1 + cnt)
+  }
+
+  def reverse[A](list: List[A]): List[A] = ???
+
+  def isPalindrome[A](list: List[A]): Boolean = ???
 }
--- src/test/scala/s99Spec.scala
+++ src/test/scala/s99Spec.scala
@@ -1,15 +1,50 @@
 import org.specs2.mutable.Specification
 
 object s99Spec extends Specification {
-  "The last function" >> {
-    "return None for Nil" >> {
+  "The 'last' function" >> {
+    "returns None for Nil" >> {
       s99.last(Nil) must_== None
     }
-    "return Some(x) for List(x)" >> {
+    "returns Some(x) for List(x)" >> {
       s99.last(List(1)) must_== Some(1)
     }
-    "return Some(n) for List(1,...,n)" >> {
+    "returns Some(n) for List(1,...,n)" >> {
       s99.last(List(1,2,3)) must_== Some(3)
+    }
+  }
+
+  "The 'penultimate' function" >> {
+    "returns None for Nil" >> {
+      s99.penultimate(Nil) must_== None
+    }
+    "returns None for List(x)" >> {
+      s99.penultimate(List(1)) must_== None
+    }
+    "returns Some(m) for List(1,...,m,n)" >> {
+      s99.penultimate(List(1,2,3)) must_== Some(2)
+    }
+  }
+
+  "The 'nth' function" >> {
+    "returns None for n > length(list)" >> {
+      s99.nth(2, List(1)) must_== None
+    }
+    "returns None for Nil (same as above)" >> {
+      s99.nth(2, Nil) must_== None
+    }
+    "returns nth element" >> {
+      s99.nth(2, List(0,1,2,3)) must_== Some(2)
+      s99.nth(2, List(0,1,2)) must_== Some(2)
+    }
+  }
+
+  "The 'length' function" >> {
+    "returns 0 for Nil" >> {
+      s99.length(Nil) must_== 0
+    }
+    "returns n for List(1,...,n)" >> {
+      s99.length(List(1)) must_== 1
+      s99.length(List(1,2,3)) must_== 3
     }
   }
 }
