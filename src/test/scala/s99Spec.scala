080be2f9e24f136fce06545b49769279ad3b4dc5
Dean Hudson <dean@soundcloud.com>
2016-09-22 08:48:48 +0200

    Probs through S08, compress

--- src/test/scala/s99Spec.scala
+++ src/test/scala/s99Spec.scala
@@ -51,9 +51,11 @@ object s99Spec extends Specification {
   "5: The 'reverse' function" >> {
     "returns Nil for Nil" >> {
       s99.reverse(Nil) must_== Nil
+      s99.reverseTail(Nil) must_== Nil
     }
     "returns List(n,...,1) for List(1,...,n)" >> {
       s99.reverse(List(1,2,3)) must_== List(3,2,1)
+      s99.reverseTail(List(1,2,3)) must_== List(3,2,1)
     }
   }
 
@@ -79,15 +81,42 @@ object s99Spec extends Specification {
   "7: The 'flatten' function" >> {
     "returns Nil for Nil" >> {
       s99.flatten(Nil) must_== Nil
+      s99.flattenFM(Nil) must_== Nil
     }
     "returns List(1) for List(1)" >> {
       s99.flatten(List(1)) must_== List(1)
+      s99.flattenFM(List(1)) must_== List(1)
     }
     "returns List(1) for List(List(1))" >> {
       s99.flatten(List(List(1))) must_== List(1)
+      s99.flattenFM(List(List(1))) must_== List(1)
     }
     "returns List(1,2) for List(1, List(2))" >> {
       s99.flatten(List(1, List(2))) must_== List(1,2)
+      s99.flattenFM(List(1, List(2))) must_== List(1,2)
+    }
+  }
+
+  "8: The 'compress' function" >> {
+    "returns Nil for Nil" >> {
+      s99.compress(Nil) must_== Nil
+      s99.compressTail(Nil) must_== Nil
+    }
+    "returns List(1) for List(1)" >> {
+      s99.compress(List(1)) must_== List(1)
+      s99.compressTail(List(1)) must_== List(1)
+    }
+    "returns List(1,...,n) for List(1,...,n)" >> {
+      s99.compress(List(1,2,3,4)) must_== List(1,2,3,4)
+      s99.compressTail(List(1,2,3,4)) must_== List(1,2,3,4)
+    }
+    "returns List(1) for List(1,1,...,1)" >> {
+      s99.compress(List(1,1,1,1,1)) must_== List(1)
+      s99.compressTail(List(1,1,1,1,1)) must_== List(1)
+    }
+    "returns List(1,2) for List(1,1,2,2)" >> {
+      s99.compress(List(1,1,2,2)) must_== List(1,2)
+      s99.compressTail(List(1,1,2,2)) must_== List(1,2)
     }
   }
 }
