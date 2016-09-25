82cc300fa011b20906e02bf455257f16262c8a3d
Dean Hudson <dean@soundcloud.com>
2016-09-25 22:57:37 +0200

    S09

--- src/main/scala/s09.scala
+++ src/main/scala/s09.scala
@@ -2,5 +2,12 @@ import annotation.tailrec
 
 object s09 {
   // S09
-  def pack = ???
+  def pack[A](list: List[A]): List[List[A]] = list match {
+    case Nil      => Nil
+    case h :: Nil => List(List(h))
+    case h :: t   => {
+      val (hs, rest) = list.span(_ == h)
+      hs :: pack(rest)
+    }
+  }
 }
--- src/test/scala/s09Spec.scala
+++ src/test/scala/s09Spec.scala
@@ -2,8 +2,23 @@ import org.specs2.mutable.Specification
 
 object s09Spec extends Specification {
   "S09: The 'pack' function" >> {
-    "has a test" >> {
-      true must_== false
+    "returns Nil for Nil" >> {
+      s09.pack(Nil) must_== Nil
+    }
+    "returns List(List(1)) for List(1)" >> {
+      s09.pack(List(1)) must_== List(List(1))
+    }
+    "returns List(List(1, 1)) for List(1, 1)" >> {
+      s09.pack(List(1, 1)) must_== List(List(1, 1))
+    }
+    "returns List(List(1, 1, 1)) for List(1, 1, 1)" >> {
+      s09.pack(List(1, 1, 1)) must_== List(List(1, 1, 1))
+    }
+    "returns List(List(1), List(2, 2)) for List(1, 2, 2)" >> {
+      s09.pack(List(1, 2, 2)) must_== List(List(1), List(2, 2))
+    }
+    "returns List(List(1, 1), List(2)) for List(1, 1, 2)" >> {
+      s09.pack(List(1, 1, 2)) must_== List(List(1, 1), List(2))
     }
   }
 }
